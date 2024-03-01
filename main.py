6f98668507f09572b84ca3fae7194e8d6063a47b
irohanrajput <itusharsingh27@gmail.com>
2024-03-01 05:33:08 +0530

    changed some logics, and added css.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request
 import openai
 import json
 
-with open('/path/to/apikey.json') as f:
-    api_key = json.load(f)['api_key']
-#create a file called 'apikey.json' in your root direvtory and add your api key in it
+with open('apikey.json') as f:
+    api_key = json.load(f)['key']
+    
+#create a file 'apikey.json' in the root dir and add  api key in it
 #Sample: {"key": "YOUR_API_KEY_HERE"}
 #sayonarraaaa
 
@@ -12,26 +13,32 @@ openai.api_key = api_key
 
 app = Flask(__name__)
 
+chat_history = []
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", chat_history=chat_history)
 
 @app.route("/get_response", methods=["POST"])
 def get_response():
+    
     user_prompt = request.form["user_input"]
     prompt_to_be_sent = f"You: {user_prompt}\nAssistant: "
-    
     
     response = openai.completions.create(
         model="gpt-3.5-turbo-instruct",
             prompt=prompt_to_be_sent,
             max_tokens=100,
-            temperature=0
+            temperature=0.7
             )
-    
     assistant_response = response.choices[0].text.strip()
+    
+    chat_history.append({"user_text":user_prompt, "response_text": assistant_response})
+    chat_history.reverse()  # Reverse the order of elements in chat_history
+    
+    
 
-    return render_template("index.html", user_prompt=user_prompt, response_text=assistant_response)
+    return render_template("index.html", chat_history=chat_history)
 
 if __name__ == "__main__":
     app.run(debug=True)
